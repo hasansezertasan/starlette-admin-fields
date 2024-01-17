db365ec7fb391a44d4e7a3c45cd6f06280079404
Hasan Sezer Taşan <13135006+hasansezertasan@users.noreply.github.com>
2024-01-17 08:31:54 +0300

    Content of PRs made to `starlette-admin` repository. (#1)
    Add PR content

---
+++ starlette_admin_fields/statics/js/form-extra.js
@@ -0,0 +1,36 @@
+(function () {
+    function processCustomElement(element) {
+        // CKEditor4Field integration
+        $(".field-ckeditor4", element).each(function () {
+            let options = $(this).data("options");
+            CKEDITOR.replace(this, options);
+        });
+        // end CKEditor4Field integration
+
+        // CKEditor5Field integration
+        $(".field-ckeditor5", element).each(function () {
+            ClassicEditor
+                .create(
+                    this,
+                )
+                .catch(error => {
+                    console.error(error);
+                });
+        });
+        // end CKEditor5Field integration
+    
+        // SimpleMDEField integration
+        $(".field-simplemde", element).each(function () {
+            let options = $(this).data("options");
+            new SimpleMDE({
+                element: this,
+                ...options
+            });
+        });
+        // end SimpleMDEField integration
+
+    }
+    $(function () {
+        processCustomElement(document);
+    });
+})();
