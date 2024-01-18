b6d1c7e0abf085fdd0d5fa5123c0277927fcedeb
hasansezertasan <hasansezertasan@gmail.com>
2024-01-18 12:07:44 +0300

    SimpleMDEField, CKEditor4Field and CKEditor5Field are ready.

---
+++ starlette_admin_fields/fields/ckeditor5.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from dataclasses import field as dc_field
+from typing import List
+
+from starlette.requests import Request
+from starlette_admin._types import RequestAction
+from starlette_admin.fields import TextAreaField
+
+@dataclass
+class CKEditor5Field(TextAreaField):
+    """A field that provides a WYSIWYG editor for long text content using the
+     [CKEditor5](https://ckeditor.com/) library.
+
+    This field can be used as an alternative to the [TextAreaField][starlette_admin.fields.TextAreaField]
+    to provide a more sophisticated editor for user input.
+    """
+
+    class_: str = "field-ckeditor5 form-control"
+    display_template: str = "displays/tinymce.html"
+    version: str = "40.0.0"
+    distribution: str = "classic"
+
+    def additional_js_links(self, request: Request, action: RequestAction) -> List[str]:
+        if action.is_form():
+            return [
+                f"https://cdn.ckeditor.com/ckeditor5/{self.version}/{self.distribution}/ckeditor.js",
+                str(request.url_for(f"{request.app.state.ROUTE_NAME}:statics", path="js/form-extra.js")),
+            ]
+        return []
+
+    def additional_css_links(self, request: Request, action: RequestAction) -> List[str]:
+        if action.is_form():
+            return [str(request.url_for(f"{request.app.state.ROUTE_NAME}:statics", path="css/ckeditor5.css"))]
+        return []
