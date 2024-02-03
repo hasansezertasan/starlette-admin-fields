b921dbd544cb630b497560339faf1ed87e527da7
Hasan Sezer Taşan <13135006+hasansezertasan@users.noreply.github.com>
2024-02-04 02:33:52 +0300

    Rename `extend_admin` as `StarletteAdminFields` and add placeholder template. (#29)
    * Rename 'extend_admin' as 'StarletteAdminFields' and add placeholder template.[201~
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- starlette_admin_fields/base.py
+++ starlette_admin_fields/base.py
@@ -1,30 +1,62 @@
+from typing import Optional
+
 from jinja2 import PackageLoader
 from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
 from starlette_admin import BaseAdmin
 
 
-def extend_admin(admin: BaseAdmin) -> None:
+class StarletteAdminFields:
     """
-    Extend admin with extra templates and statics.
+    Starlette Admin Fields.
 
-    Parameters:
-        admin: The admin instance.
-
-    !!! usage
-        ```python
-        from starlette_admin_fields import extend_admin
-
-        extend_admin(admin)
-        ```
+    !!! note
+        This class is just a placeholder for the documentation.
     """
-    admin.templates.env.loader.loaders.append(  # type: ignore
-        PackageLoader("starlette_admin_fields", "templates")
-    )
-    admin.routes.append(
-        Mount(
-            "/statics-saf",
-            app=StaticFiles(packages=[("starlette_admin_fields", "statics")]),
-            name="statics-saf",
+
+    def __init__(self, admin: Optional[BaseAdmin] = None) -> None:
+        """
+        Initialize Starlette Admin Fields.
+
+        Parameters:
+            admin: The admin instance.
+
+        !!! usage
+            ```python
+            from starlette_admin import BaseAdmin
+            from starlette_admin_fields import StarletteAdminFields
+
+            admin = BaseAdmin()
+            saf = StarletteAdminFields(admin)
+            ```
+        """
+        if admin is not None:
+            self.init_admin(admin)
+
+    def init_admin(self, admin: BaseAdmin) -> None:
+        """
+        Initialize admin with extra templates and statics.
+
+        Parameters:
+            admin: The admin instance.
+
+        !!! usage
+            ```python
+            from starlette_admin import BaseAdmin
+            from starlette_admin_fields import StarletteAdminFields
+
+            admin = BaseAdmin()
+            saf = StarletteAdminFields()
+            saf.init_admin(admin)
+            ```
+        """
+        admin.templates.env.loader.loaders.append(  # type: ignore
+            PackageLoader("starlette_admin_fields", "templates")
         )
-    )
+        admin.routes.append(
+            Mount(
+                "/statics-saf",
+                app=StaticFiles(packages=[("starlette_admin_fields", "statics")]),
+                name="statics-saf",
+            )
+        )
